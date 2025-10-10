8077d48717f5a777efa36b5df366bb4487865032
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2025-10-09 20:11:01 -0700

    feat: configured dnd manager module for test page

--- modules/dnd-manager/src/DndManagerModule.ts
+++ modules/dnd-manager/src/DndManagerModule.ts
@@ -1,13 +1,25 @@
 import { NativeModule, requireNativeModule } from "expo";
 
-import { DndManagerModuleEvents, DndState } from "./DndManager.types";
+import {
+  AppInfo,
+  DndManagerModuleEvents,
+  DndState,
+  InterruptionFilterConstants,
+  NotificationPermissionResult,
+} from "./DndManager.types";
 
 declare class DndManagerModule extends NativeModule<DndManagerModuleEvents> {
   hasPermission(): boolean;
   requestPermission(): Promise<boolean>;
   getCurrentState(): DndState;
   setDNDEnabled(enabled: boolean): Promise<DndState>;
+  setInterruptionFilter(filter: number): Promise<DndState>;
+  getAllInstalledApps(includeSystemApps: boolean): Promise<AppInfo[]>;
+  getAppNotificationStatus(
+    packageName: string
+  ): Promise<NotificationPermissionResult>;
+  openAppNotificationSettings(packageName: string): Promise<boolean>;
+  getInterruptionFilterConstants(): InterruptionFilterConstants;
 }
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<DndManagerModule>("DndManager");
