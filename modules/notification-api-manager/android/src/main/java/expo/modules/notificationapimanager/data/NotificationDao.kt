49649a2c5a861e47085b2ca63ae3b55cec4d75a5
Arshia Agarwal <165864696+asagarw4@users.noreply.github.com>
2026-03-17 23:38:18 -0700

    Create NotificationDao.kt

---
+++ modules/notification-api-manager/android/src/main/java/expo/modules/notificationapimanager/data/NotificationDao.kt
@@ -0,0 +1,14 @@
+@Dao
+interface NotificationDao (
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNotifications(vararg notification: NotificationEntity)
+    
+    @Update
+    suspend fun updateNotifications(vararg notification: NotificationEntity)
+
+    @Delete
+    suspend fun deleteNotifications(vararg notification: NotificationEntity)
+
+    @Query("SELECT * FROM notification_db")
+    suspend fun loadAll_notifications() : FLOW<List<NotificationEntity>>
+    )
