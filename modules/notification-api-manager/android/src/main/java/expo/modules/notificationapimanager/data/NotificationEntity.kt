74179a4937746ffbed8b38b851aa27f4ea8e1c4f
Arshia Agarwal <165864696+asagarw4@users.noreply.github.com>
2026-03-17 22:35:05 -0700

    Create NotificationEntity.kt
    Entity class of the database, describes SQL table name and layout

---
+++ modules/notification-api-manager/android/src/main/java/expo/modules/notificationapimanager/data/NotificationEntity.kt
@@ -0,0 +1,11 @@
+ @Entity(tableName = "notification_db")
+data class NotificationEntity (
+     @PrimaryKey(autoGenerate = true)
+     val id: Int = 0,
+     val key: String,
+     val packageName: String,
+     val appName: String,
+     val title: String,
+     val content: ByteArray?,
+     val timestamp: Long
+)
