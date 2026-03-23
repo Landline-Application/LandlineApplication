92384c794d464422d5bd7d972973bd598c46a129
Arshia Agarwal <165864696+asagarw4@users.noreply.github.com>
2026-03-22 19:53:34 -0700

    Update NotificationRepository.kt

--- modules/notification-api-manager/android/src/main/java/expo/modules/notificationapimanager/repository/NotificationRepository.kt
+++ modules/notification-api-manager/android/src/main/java/expo/modules/notificationapimanager/repository/NotificationRepository.kt
@@ -1,1 +1,16 @@
+import expo.modules.notificationapimanager.data.*
+
+class NotificationRepository(private val NotificationDao: dao) {
+
+  val readAll_notifications: LiveData<List<NotificationEntitity>> = dao.loadAll_notifications()
+  
+  fun add_notification(NotificationEntitity) : Boolean {
+        try
+        {
+            dao.insertNotifications(notification)
+            true
+        }
+        catch
+
+}
 
