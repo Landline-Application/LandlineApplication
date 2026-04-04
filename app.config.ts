5621ec3fd3f0f0ee3b3efaea86664d388f31f746
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2026-04-04 13:53:28 -0700

    feat(assets): use app icon for splash screen

--- app.config.ts
+++ app.config.ts
@@ -46,7 +46,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     [
       'expo-splash-screen',
       {
-        image: './assets/images/splash-icon.png',
+        image: './assets/landline-icon.png',
         imageWidth: 200,
         resizeMode: 'contain',
         backgroundColor: '#ffffff',
